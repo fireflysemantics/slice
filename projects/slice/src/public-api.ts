4105e10d7a7591ee3a88281da89f8d04f7ec6ce3
Ole Ersoy <ole.ersoy@gmail.com>
2021-08-05 17:27:34 -0400

    Updated the API

--- projects/slice/src/public-api.ts
+++ projects/slice/src/public-api.ts
@@ -2,3 +2,8 @@
  * Public API Surface of slice
  */
 export * from './lib/models/'
+export * from './lib/AbstractStore'
+export * from './lib/EStore'
+export * from './lib/OStore'
+export * from './lib/Slice'
+export * from './lib/utilities'
