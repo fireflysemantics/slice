4ae3acf5347846e3533957fee12dfb00f97e94b1
Ole Ersoy <ole.ersoy@gmail.com>
2021-11-18 15:50:52 -0500

    Added constants back in

---
+++ projects/slice/src/lib/models/constants.ts
@@ -0,0 +1,2 @@
+export const SCROLL_UP_DEBOUNCE_TIME_20 = 20;
+export const SEARCH_DEBOUNCE_TIME_300 = 300;
--- projects/slice/src/lib/models/index.ts
+++ projects/slice/src/lib/models/index.ts
@@ -2,3 +2,5 @@ export * from './ActionTypes'
 export * from './Delta'
 export * from './Predicate'
 export * from './StoreConfig'
+export * from './Entity'
+export * from "./constants"
