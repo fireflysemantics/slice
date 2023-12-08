85c4001f8a6d4024d14604eb83a1c43dc165628f
Ole Ersoy <ole.ersoy@gmail.com>
2023-12-08 08:48:37 -0500

    Generate slice 17 workspace project

---
+++ projects/slice/src/lib/slice.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-slice',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      slice works!
+    </p>
+  `,
+  styles: ``
+})
+export class SliceComponent {
+
+}
---
+++ projects/slice/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of slice
+ */
+
+export * from './lib/slice.service';
+export * from './lib/slice.component';
